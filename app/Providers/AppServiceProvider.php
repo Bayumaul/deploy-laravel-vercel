bfba94a64e560624837c0d4eaca991a6faad17e8
Bayumaul <bayu.maulanaikhsan123@gmail.com>
2024-01-09 09:55:04 +0700

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        URL::forceScheme('https');
+    }
+}
